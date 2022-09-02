8cdfae92b545f6d40b8b54ee5fdd9152f9a4d9e6
Samuel Ribeiro Batista <leidymotastz@outlook.com>
2022-09-02 20:37:09 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Declaração Goto
+
+            //    Inicil:
+
+            //    Console.Write("Esolha uma Opção :  ");
+            //    int op = int.Parse(Console.ReadLine());
+            //    int Valor = 0;
+
+            //    switch(op)
+            //    {
+            //        default:
+            //            goto  Inicil ;
+            //            break;
+            //        case 1:
+            //            Valor += 100;
+            //            break;
+            //        case 2:
+            //            Valor += 50;
+            //            goto case 1;
+
+            //    }
+
+
+
+            //    Console.WriteLine("Valor final "+ Valor );
+
+            //    Console.ReadKey();
+
+            //}
+
+            #endregion
+        }
+        
+    }
+
+  
+
+
+
+
+
+
+
+
+
+
+            
+    
+      
+
+
+
+
+
+    
+
+
+
+            
+        
+
+
+    
+
+
+
+
+
+
+
